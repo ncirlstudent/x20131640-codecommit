4ff39d78464c76887ede57d4f75df127b4d87c4d
EL JOHRI Hamza <Hamza.ELJOHRI@accor.com>
2023-12-13 12:29:14 +0100

    Fix static pages

--- eshop/settings.py
+++ eshop/settings.py
@@ -142,9 +142,10 @@ USE_TZ = True
 # STATICFILES_DIRS = (
 #   os.path.join(BASE_DIR, 'static/'),
 # )
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_URL = '/static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
