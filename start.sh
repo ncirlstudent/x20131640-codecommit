5322dd6beca29d9238d9e94ec8bb95eec1404220
EL JOHRI Hamza <Hamza.ELJOHRI@accor.com>
2023-12-13 09:37:10 +0100

    update ec2 ip

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 # Navigate to your Django project directory
 cd /home/ec2-user/app
 
-# Activate virtual environment if you have one
-source venv/bin/activate
-
 # Start Django server in the background
 nohup python3 manage.py runserver 0.0.0.0:8080 >/dev/null 2>&1 &
 python3 manage.py collectstatic --noinput
