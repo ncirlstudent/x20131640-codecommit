842fc23ca32da7ab17cf5f0760af63df152d7b93
EL JOHRI Hamza <Hamza.ELJOHRI@accor.com>
2023-12-13 09:45:48 +0100

    update ec2 ip

--- eshop/settings.py
+++ eshop/settings.py
@@ -140,9 +140,11 @@ USE_TZ = True
 # STATICFILES_DIRS = (
 #   os.path.join(BASE_DIR, 'static/'),
 # )
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 #STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
