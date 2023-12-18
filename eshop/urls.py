be393b3afe337ab2374354f6b3ab259cc53cab10
ncirlstudent <141283690+ncirlstudent@users.noreply.github.com>
2023-12-18 20:16:19 +0530

    Update urls.py

--- eshop/urls.py
+++ eshop/urls.py
@@ -19,12 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('product/', include('product.urls')),
     path('accounts/', include('accounts.urls')),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
