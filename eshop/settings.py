d35cb6ccc27c474a8473fba406008dd759016771
arjunjadhav <x20131640@student.ncirl.ie>
2023-12-19 13:15:10 +0000

    title added

--- eshop/settings.py
+++ eshop/settings.py
@@ -151,7 +151,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
